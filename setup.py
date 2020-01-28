0d241bb1f89813ab75d87830bc6f9da3d475d120
Tan Nian Wei <tannianwei@aggienetwork.com>
2020-01-28 20:15:02 +0800

    Added dependencies in pip

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open('README.md', "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='python-rle', 
+    version="0.0.2",
+    author="Tan Nian Wei",
+    author_email="tannianwei@aggienetwork.com",
+    description="Run-length encoding for data analysis in Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tnwei/pyrle",
+    packages=setuptools.find_packages(),
+    install_requires=[
+          "joblib",
+      ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
